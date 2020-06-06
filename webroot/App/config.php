4e9b8a4acced6f5de3d1efb373639ce6569e4032
Olga Vara Pérez <olgavara14@gmail.com>
2020-06-06 16:52:04 +0200

    Modifico configuracion de acceso a bd

--- webroot/App/config.php
+++ webroot/App/config.php
@@ -1,7 +1,10 @@
 <?php 
 //------------Conexion a BD--------------
-define ( 'DBSERVER' , 'localhost' );
-define ( 'DBNAME' , 'roomview' );
-define ( 'DBUSER' , 'root' );
-define ( 'DBPASSWORD' , 'root' );
+
+
+define ( 'DBSERVER' , $_ENV['MYSQL_SERVICE_HOST'] );
+define ( 'DBNAME' , $_ENV['DATABASE_NAME'] );
+define ( 'DBUSER' , $_ENV['DATABASE_USER'] );
+define ( 'DBPASSWORD' , $_ENV['DATABASE_PASSWORD'] );
+
 ?>
